db6ed215fa56a548a77194afd7766470f02a24b2
shaon07 <121743010+shaon07softic@users.noreply.github.com>
2023-08-23 15:20:07 +0600

    [add] new design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,39 +8,29 @@ export default async function Home() {
   const users = await getUser();
 
   return (
-    <main className="flex min-h-screen flex-col items-center gap-5 p-24">
-      <h1 className="text-5xl font-bold">Welcome to NextJS</h1>
-      <h2>Hello World</h2>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium ut natus cumque quia
-        eos sapiente placeat dolorem deleniti. Consequuntur cumque inventore commodi reprehenderit
-        nobis obcaecati reiciendis? Dolor consequatur aspernatur maxime. Laboriosam deleniti vitae,
-        ad, velit beatae voluptatem, fuga rerum optio amet eius esse dicta dolorum minus omnis
-        voluptate unde dolores labore nisi quia iure. Laborum molestias, distinctio similique nam
-        voluptatum optio iure, pariatur placeat maxime laudantium nulla asperiores veniam accusamus
-        quia sequi amet tempora, sunt ad nemo sapiente modi alias a ea! Nobis, excepturi voluptate?
-        Nemo ipsa, tenetur rerum ducimus nisi eaque officia molestiae illum, vitae, non repudiandae
-        architecto? Id vel, accusantium, provident quos odit accusamus beatae quo recusandae unde
-        debitis, asperiores vitae vero facere magnam fuga similique enim optio praesentium quam!
-        Dolorem, sapiente quas. Recusandae voluptate ducimus aperiam neque maiores porro sequi
-        exercitationem nisi eligendi enim ullam ipsam dolorem beatae, quas corrupti quisquam at,
-        doloremque expedita, voluptatem quo rerum quam minima dolorum error. Earum iure error
-        molestias dolor voluptatum officiis modi. Facere, culpa dolores? Molestias tempora nesciunt,
-        deserunt ab obcaecati repudiandae illo optio ratione, iusto repellat omnis ducimus dolorum
-        aspernatur fugit fugiat rerum ipsam quos quas pariatur vero. Iusto voluptatem et vero alias
-        quibusdam numquam reiciendis inventore, voluptate quas.
-      </p>
-
-      <h1>AWS ADDED</h1>
-
-      <div className="mt-10">
-        <h2>Users</h2>
-        <ul>
-          {users.map((user: any) => (
-            <li key={user.id}>{user.name}</li>
-          ))}
-        </ul>
-      </div>
+    <main className="flex flex-col items-center">
+      {users.map((user: any, index: number) => (
+        <figure
+          key={index}
+          className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800 max-w-sm m-5"
+        >
+          {/* <Image s className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/sarah-dayan.jpg" alt="" width="384" height="512" /> */}
+          <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+            <blockquote>
+              <p className="text-lg font-medium">
+                “Tailwind CSS is the only framework that I've seen scale on large teams. It’s easy
+                to customize, adapts to any design, and the build size is tiny.”
+              </p>
+            </blockquote>
+            <figcaption className="font-medium">
+              <div className="text-sky-500 dark:text-sky-400">{user?.name}</div>
+              <div className="text-slate-700 dark:text-slate-500">
+                {user?.company?.name}, {user?.address?.city}
+              </div>
+            </figcaption>
+          </div>
+        </figure>
+      ))}
     </main>
   );
 }
