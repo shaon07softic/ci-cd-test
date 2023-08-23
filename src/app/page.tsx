3668b1a6e376a7a182a22f6b05d320a006fa5a8b
shaon07 <121743010+shaon07softic@users.noreply.github.com>
2023-08-23 10:06:16 +0600

    add test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,25 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-5xl font-bold">Welcome to NextJS</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium ut natus cumque quia
+        eos sapiente placeat dolorem deleniti. Consequuntur cumque inventore commodi reprehenderit
+        nobis obcaecati reiciendis? Dolor consequatur aspernatur maxime. Laboriosam deleniti vitae,
+        ad, velit beatae voluptatem, fuga rerum optio amet eius esse dicta dolorum minus omnis
+        voluptate unde dolores labore nisi quia iure. Laborum molestias, distinctio similique nam
+        voluptatum optio iure, pariatur placeat maxime laudantium nulla asperiores veniam accusamus
+        quia sequi amet tempora, sunt ad nemo sapiente modi alias a ea! Nobis, excepturi voluptate?
+        Nemo ipsa, tenetur rerum ducimus nisi eaque officia molestiae illum, vitae, non repudiandae
+        architecto? Id vel, accusantium, provident quos odit accusamus beatae quo recusandae unde
+        debitis, asperiores vitae vero facere magnam fuga similique enim optio praesentium quam!
+        Dolorem, sapiente quas. Recusandae voluptate ducimus aperiam neque maiores porro sequi
+        exercitationem nisi eligendi enim ullam ipsam dolorem beatae, quas corrupti quisquam at,
+        doloremque expedita, voluptatem quo rerum quam minima dolorum error. Earum iure error
+        molestias dolor voluptatum officiis modi. Facere, culpa dolores? Molestias tempora nesciunt,
+        deserunt ab obcaecati repudiandae illo optio ratione, iusto repellat omnis ducimus dolorum
+        aspernatur fugit fugiat rerum ipsam quos quas pariatur vero. Iusto voluptatem et vero alias
+        quibusdam numquam reiciendis inventore, voluptate quas.
+      </p>
     </main>
   );
 }
